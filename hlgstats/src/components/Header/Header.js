5d4a1d17583b0cca084108acd768a53eb528a55a
Cristian <cris178@outlook.com>
2020-04-15 00:10:58 -0700

    Search bar works

--- hlgstats/src/components/Header/Header.js
+++ hlgstats/src/components/Header/Header.js
@@ -2,18 +2,50 @@ import React from 'react';
 import "./Header.css";
 import "./Hamburger/Hamburger";
 import Hamburger from './Hamburger/Hamburger';
+import Search from "../Search/Search"
 
 class Header extends React.Component{
 
-    render(){
 
+    constructor(props){
+        super(props);
+        this.state ={
+            clicked: 0,
+            ign: ""
+        };
+       this.handleClick = this.handleClick.bind(this);
+       this.setIGN = this.setIGN.bind(this);
+    }
+
+    handleClick(){
+        console.log("Search Clicked")
+        this.setState({
+            clicked: 1
+        });
+        return;
+    }
+
+    setIGN(arg) {
+        //Can't console log after doesn't show up as updated until after
+        console.log("Setting State IGN: " + arg);
+        this.setState({
+          ign: arg,
+          clicked: 0
+        });
+    }
+
+    render(){
+        var standardHeader = <ul>
+                                <h1 className="left ">HLG <span className="blue">STATS</span>
+                                </h1><div className="searchIcon" onClick={this.handleClick}>&#9906;</div>
+                                <Hamburger />
+                            </ul>;
+        if(this.state.clicked === 1){
+            standardHeader = <Search passUp={this.setIGN}/>
+        }
         return(
             <div className= "Header">
-                <ul>
-                    <h1 className="left ">HLG <span className="blue">Stats</span></h1>
-                    <Hamburger />
-                   
-                </ul>
+                {standardHeader}
             </div>
         );
     }
---
+++ hlgstats/src/components/Search/Search.js
@@ -0,0 +1,56 @@
+import React from "react";
+import "./Search.css"
+
+
+class Search extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state={
+            username: ""
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    componentDidMount() {
+        console.log("Search Bar has Mounted.");
+    }
+
+
+    handleChange(event) {
+        event.preventDefault();
+        console.log("Logging Change In: " + event.target.name); //Shows which input is currently changging
+        console.log(event.target.value); //Shows the value of that specific input bar
+        this.setState({
+          username: event.target.value
+        });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        let data = this.state.username;
+        console.log("Submitted Username: " + data);
+        this.props.passUp(data);
+        //console.log(this.state.city);
+    }
+
+
+
+    render() {
+        return (
+          <div className="Search">
+            <form id="search-form">
+                <div class="searchBar">
+                    <input type="text" name="searchBar" className="round" placeholder="Search IGN" onChange={this.handleChange}/>
+                    <input type="submit" className="corner" value="&#9906;" onClick={this.handleSubmit}/>
+                </div>
+            </form>
+          </div>
+        );
+      }
+
+
+}
+
+export default Search;
